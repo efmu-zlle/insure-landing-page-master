596a5b5fecd120179cdb8038aead2818185840bc
Elmer Francisco M <74124971+efmu-zlle@users.noreply.github.com>
2023-06-09 21:44:50 -0600

    (featured) mobile and desktop completed

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import "./assets/modern-normalize.css";
 import "./assets/style.css";
 import "./assets/components/header.css";
 import "./assets/components/hero.css";
+import "./assets/components/featured.css";
 import "./assets/utils.css";
