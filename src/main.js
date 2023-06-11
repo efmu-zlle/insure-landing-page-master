485dbc5bc98f7c9c7375a5ce53c13e0f6464e845
Elmer Francisco M <74124971+efmu-zlle@users.noreply.github.com>
2023-06-10 19:48:15 -0600

    (footer) working on the design

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import "./assets/style.css";
 import "./assets/components/header.css";
 import "./assets/components/hero.css";
 import "./assets/components/featured.css";
+import "./assets/components/about.css";
+import "./assets/components/footer.css";
 import "./assets/utils.css";
